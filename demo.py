5da697329b2a6b562dd1a8d6494c9540770665b7
dqj5182 <dqj5182@snu.ac.kr>
2024-02-13 14:16:04 +0000

    Added VAE demo code

--- demo.py
+++ demo.py
@@ -11,23 +11,30 @@
 """
 import os
 import clip
+import time
 import torch
 from PIL import Image
 from default_config import cfg as config
 from models.lion import LION
 from utils.vis_helper import plot_points
-from huggingface_hub import hf_hub_download 
-import utils.yacs_config
+from models.vae_adain import Model as VAE
 
 # model_path = './lion_ckpt/unconditional/airplane/checkpoints/model.pt'
 model_path = './lion_ckpt/unconditional/all55/checkpoints/epoch_10999_iters_2100999.pt'
+# model_path = './lion_ckpt/unconditional/all55/checkpoints/vae_only.pt'
 # model_config = './lion_ckpt/unconditional/airplane/cfg.yml'
 model_config = './lion_ckpt/unconditional/all55/cfg.yml'
 
+num_sample = 10
+
 config.merge_from_file(model_config)
-lion = LION(config)
-torch.load(model_path)
-lion.load_model(model_path)
+if 'vae_only' in model_path:
+    lion = VAE(config).cuda()
+    ckpt = torch.load(model_path)
+    lion.load_state_dict(ckpt['model'])
+else:
+    lion = LION(config)
+    lion.load_model(model_path)
 
 if config.clipforge.enable:
     input_t = ["a swivel chair, five wheels"] 
@@ -41,9 +48,19 @@ if config.clipforge.enable:
     print('clip_feat', clip_feat.shape)
 else:
     clip_feat = None
-output = lion.sample(10 if clip_feat is None else clip_feat.shape[0], clip_feat=clip_feat)
-pts = output['points']
+
+start_time = time.time()
+if 'vae_only' in model_path:
+    # decomposed_eps = [torch.rand(num_sample, 128, 1, 1), torch.rand(num_sample, 8192, 1, 1)] # [0]: global prior, [1]: local prior
+    output = lion.sample(num_sample)
+    pts = output
+else:
+    output = lion.sample(num_sample if clip_feat is None else clip_feat.shape[0], clip_feat=clip_feat)
+    pts = output['points']
+end_time = time.time()
+
+print("Time elapsed:", str(end_time - start_time))
+
 img_name = "demo.png"
 plot_points(pts, output_name=img_name)
 img = Image.open(img_name)
-# img.show()
